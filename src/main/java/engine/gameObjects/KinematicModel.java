a59889658387d638f5092ea8df09a0a5fc1edf41
Cifruktus <9028029+Cifruktus@users.noreply.github.com>
2022-07-23 23:07:51 +0300

    KinematicModel fix

--- src/main/java/engine/gameObjects/KinematicModel.java
+++ src/main/java/engine/gameObjects/KinematicModel.java
@@ -5,11 +5,11 @@ import engine.GameObject;
 import math.Vector2d;
 
 public class KinematicModel extends GameObject {
-    double mass = 1;
-    double mInertia = 1;
+    public double mass = 1;
+    public double mInertia = 1;
 
-    Vector2d velocity = new Vector2d();
-    double angularVelocity = 0;
+    public Vector2d velocity = new Vector2d();
+    public double angularVelocity = 0;
 
     public KinematicModel(Vector2d pos) {
         super(pos);
@@ -18,15 +18,16 @@ public class KinematicModel extends GameObject {
     @Override
     public void update(double dt, Environment environment) {
         pos = pos.add(velocity.mul(dt));
+        angle += angularVelocity * dt;
 
         super.update(dt, environment);
     }
 
-    void applyImpulse(Vector2d impulse){
+    public void applyImpulse(Vector2d impulse){
         velocity = velocity.add(impulse.div(mass));
     }
 
-    void applyAngularImpulse(Double impulse){
+    public void applyAngularImpulse(double impulse){
         angularVelocity += impulse / mInertia;
     }
 }
